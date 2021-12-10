0a49a636be373fee0a1de8803f1a8545a723673c
HyunjuIm <dear.jjwim@gmail.com>
2021-12-10 17:28:55 +0900

    책 검색 카카오 api retrofit 연동

--- app/src/main/java/com/example/aop_part3_chapter4/MainActivity.kt
+++ app/src/main/java/com/example/aop_part3_chapter4/MainActivity.kt
@@ -23,8 +23,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getHashKey()
-
         val retrofit = Retrofit.Builder()
             .baseUrl("https://dapi.kakao.com")
             .addConverterFactory(GsonConverterFactory.create())
@@ -54,24 +52,5 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, "MainActivity - $t")
                 }
             })
-    }
-
-    private fun getHashKey() {
-        var packageInfo: PackageInfo? = null
-        try {
-            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-        } catch (e: PackageManager.NameNotFoundException) {
-            e.printStackTrace()
-        }
-        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
-        for (signature in packageInfo!!.signatures) {
-            try {
-                val md: MessageDigest = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
-            } catch (e: NoSuchAlgorithmException) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
-            }
-        }
     }
 }
